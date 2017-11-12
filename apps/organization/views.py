cd77cf9f9c1a0690a9f1afd26fb312724501299d
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-11-12 09:48:53 +0800

    分页与筛选

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -7,14 +7,34 @@ from .models import CourseOrg,CityDict
 
 # Create your views here.
 class OrgView(View):
+    #  此get方法用于request.GET方式请求资源
     def get(self,request):
+        # 课程机构
         all_orgs = CourseOrg.objects.all()  # SELECT * FROM courseorg
-        org_nums = all_orgs.count()  # SELECT count() FROM courseorg
+        hot_orgs = all_orgs.order_by('-click_nums')[:2]
+        # 城市
         all_cities = CityDict.objects.all()
-        #  取出筛选的城市
-        city_id = request.GET.get('city', '')  #  得到city_id值（request.GET向服务器请求数据，通常请求参数在url中直接体现）
+
+        #  城市筛选
+        city_id = request.GET.get('city', '')  #  从前端得到city_id值（request.GET向服务器请求数据，通常请求参数在url中直接体现）
         if city_id:
-            all_orgs = CourseOrg.objects.fiter(city_id=int(city_id))
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        #  类别筛选
+        category = request.GET.get('ct','')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        # 排序
+        sort = request.GET.get('sort','')
+        if sort == 'students':
+            all_orgs = all_orgs.order_by('-students')
+        if sort == 'courses':
+            all_orgs = all_orgs.order_by('-course_nums')
+
+        #  记录统计
+        org_nums = all_orgs.count()  # SELECT count() FROM courseorg
+
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -29,4 +49,8 @@ class OrgView(View):
             'all_orgs':orgs,
             'all_cities':all_cities,
             'org_nums': org_nums,
+            'city_id':city_id,
+            'category':category,
+            'hot_orgs':hot_orgs,
+            'sort':sort
         })
