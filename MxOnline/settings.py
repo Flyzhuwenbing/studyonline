57b909c4f589e4e984f8126db1eb843102175d0f
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-11-25 23:49:28 +0800

    部署

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -146,10 +146,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'smtp.sina.cn'
 EMAIL_PORT = 25
