b067d0e5336546f9ad5bfc9ade247bf95f7097b3
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-11-26 00:41:24 +0800

    部署

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -98,13 +98,22 @@ WSGI_APPLICATION = 'MxOnline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mxonline',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST':'127.0.0.1',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mxonline',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'127.0.0.1',
+        'PASSWORD': 'mysql',
+        'HOST':'120.8.201.155',
     }
 }
 
