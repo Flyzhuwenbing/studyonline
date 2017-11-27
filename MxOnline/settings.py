192d3f0f858cc0c784bf7412ea0bba7b0b8eb6fd
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-11-27 17:20:40 +0800

    readme

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -98,25 +98,25 @@ WSGI_APPLICATION = 'MxOnline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mxonline',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'127.0.0.1',
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'mxonline',
 #         'USER': 'root',
-#         'PASSWORD': 'mysql',
-#         'HOST':'localhost',
-#         'PORT':'3306',
+#         'PASSWORD': 'root',
+#         'HOST':'127.0.0.1',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxonline',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
 
 
 # Password validation
